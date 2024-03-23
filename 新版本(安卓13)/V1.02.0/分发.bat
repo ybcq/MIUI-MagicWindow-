bd8d5dcf13c5a19526a30f2996b4485a918214ba
ybcq <104081176+ybcq@users.noreply.github.com>
2024-03-23 15:28:58 +0800

    Add files via upload

---
+++ 新版本(安卓13)/V1.02.0/分发.bat
@@ -0,0 +1,24 @@
+@echo off
+
+::复制CONFIG
+xcopy /y %~d0magic_window_setting_config.xml E:\Projects\完美横屏应用计划\2.开发版\common\system\users\0\magic_window_setting_config.xml
+xcopy /y %~d0magic_window_setting_config.xml E:\Projects\完美横屏应用计划\1.稳定版(推荐)\common\system\users\0\magic_window_setting_config.xml
+xcopy /y %~d0magic_window_setting_config.xml E:\Projects\完美横屏应用计划\3.折叠屏专用\common\system\users\0\magic_window_setting_config.xml
+xcopy /y %~d0magic_window_setting_config.xml E:\Projects\完美横屏应用计划\3.红米专用\common\system\users\0\magic_window_setting_config.xml
+xcopy /y %~d0magic_window_setting_config.xml E:\Projects\完美横屏应用计划\3.6Max专用\common\system\users\0\magic_window_setting_config.xml
+
+::复制LIST
+xcopy /y %~d0drag\magicWindowFeature_magic_window_application_list.xml E:\Projects\完美横屏应用计划\2.开发版\common\system\magicWindowFeature_magic_window_application_list.xml 
+xcopy /y %~d0nodrag\magicWindowFeature_magic_window_application_list.xml  E:\Projects\完美横屏应用计划\1.稳定版(推荐)\common\system\magicWindowFeature_magic_window_application_list.xml
+xcopy /y %~d0nodrag\magicWindowFeature_magic_window_application_list.xml  E:\Projects\完美横屏应用计划\3.折叠屏专用\common\system\magicWindowFeature_magic_window_application_list.xml
+xcopy /y %~d0nodrag\magicWindowFeature_magic_window_application_list.xml  E:\Projects\完美横屏应用计划\3.红米专用\common\system\magicWindowFeature_magic_window_application_list.xml
+xcopy /y %~d0nodrag\magicWindowFeature_magic_window_application_list.xml  E:\Projects\完美横屏应用计划\3.6Max专用\common\system\magicWindowFeature_magic_window_application_list.xml
+
+::复制RULES
+xcopy /y %~d0embedded_rules_list_dbg.xml E:\Projects\完美横屏应用计划\2.开发版\common\product\etc\embedded_rules_list.xml
+xcopy /y %~d0embedded_rules_list.xml E:\Projects\完美横屏应用计划\1.稳定版(推荐)\common\product\etc\embedded_rules_list.xml
+xcopy /y %~d0embedded_rules_list_fold.xml E:\Projects\完美横屏应用计划\3.折叠屏专用\common\product\etc\embedded_rules_list.xml
+xcopy /y %~d0embedded_rules_list_redmi.xml E:\Projects\完美横屏应用计划\3.红米专用\common\product\etc\embedded_rules_list.xml
+xcopy /y %~d0embedded_rules_list_6max.xml E:\Projects\完美横屏应用计划\3.6Max专用\common\product\etc\embedded_rules_list.xml
+
+::pause
